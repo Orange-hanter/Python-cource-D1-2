b23acd639a49479f4f32ced92d96f11f9dd09c57
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2023-01-19 22:33:48 +0300

    Changes on described errors

--- very_useful_script.sh
+++ very_useful_script.sh
@@ -6,26 +6,26 @@
 #3) if arg is a number (integer number) and it is a PID of running process, print the 'cmdline' used to run this process (hint: use /proc/ subsystem), 
 #    if not a PID - add '1000' and print this sum to console
 
-# Check if the argument is an ELF executable
-if file "$1" | grep -q "ELF"; then
-    # Print the .rodata section of the file
-    readelf -S "$1" | grep '.rodata'
-
+# Check if the argument is a number
+if [[ "$1" =~ ^(-)?[0-9]+$ ]]; then
+    # Check if the argument is a PID of a running process
+    x=$(($1))
+    if [[ -d /proc/$x ]]; then
+        # Print the cmdline used to run the process
+        cat /proc/$x/cmdline
+    else
+        # Add 1000 to the argument and print the sum
+        echo $(( $x + 1000 ))
+    fi
 # Check if the argument is a .txt file
 elif [[ "$1" == *.txt ]]; then
     # Append the current time to the end of the file
     echo $(date) >> "$1"
-
-# Check if the argument is a number
-elif [[ "$1" =~ ^[0-9]+$ ]]; then
-    # Check if the argument is a PID of a running process
-    if [[ -d /proc/$1 ]]; then
-        # Print the cmdline used to run the process
-        cat /proc/$1/cmdline
-    else
-        # Add 1000 to the argument and print the sum
-        echo $(( $1 + 1000 ))
-    fi
+# Check if the argument is an ELF executable
+elif file "$1" | grep -q "ELF"; then
+    # Print the .rodata section of the file
+    readelf -x .rodata "$1"
 else
     echo "Invalid argument"
+    exit 1
 fi
