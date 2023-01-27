6e1288cf506ac14208161f44579df8c27014c74f
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2023-01-27 16:53:53 +0300

    Updated script for analyzing big files

--- big_file_statistic.py
+++ big_file_statistic.py
@@ -1,23 +1,40 @@
 from collections import Counter
+from io import TextIOWrapper
 import sys
 
 
-def read_binary_file():
+def read_data_chunk(file_object: TextIOWrapper, chunk_size : int = 1024):
+    while True:
+        data = file_object.read(chunk_size)
+        if not data:
+            break
+        else:
+            yield data
+    file_object.close()
+
+
+def get_binary_file():
     # todo implement configuration of the number of displayed elements through arguments
     if not len(sys.argv) == 2:
         raise RuntimeError("Path argument are not specified")
     path = sys.argv[1]
-    with open(path, 'rb') as file:
-        return file.read()
+    file = open(path, 'rb')
+    return read_data_chunk(file) 
 
-    
-def print_symbols_statistic(byte_string, count: int = 10) -> None:
+
+def symbols_statistic(byte_string_generator) -> Counter:
+    meta_inf = Counter()
+    for chunk in byte_string_generator:
+        meta_inf.update(chunk.decode('utf-8', errors='ignore'))
+    return meta_inf
+
+
+def print_tables(table:Counter, top_count: int = 10):
     print("Most common symbols are:")
-    meta_inf = Counter(byte_string.decode('utf-8', errors='ignore'))
-    for smb, count in meta_inf.most_common(count):
+    for smb, count in table.most_common(top_count):
         print(f"{repr(smb)} => {count} times")
 
-
 if __name__ == '__main__':
-    byte_string  = read_binary_file()
-    print_symbols_statistic(byte_string )
+    byte_string_gen  = get_binary_file()
+    result = symbols_statistic(byte_string_gen)
+    print_tables(result)
