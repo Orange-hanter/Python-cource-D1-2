af50940bcadeadf7aa81e5e44fb15e29c60c7569
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2023-01-31 21:23:38 +0300

    temporary solution

--- big_file_statistic.py
+++ big_file_statistic.py
@@ -3,9 +3,10 @@ from io import TextIOWrapper
 import sys
 
 
-def read_data_chunk(file_object: TextIOWrapper, chunk_size : int = 1024):
+def read_data_chunk(file_object: TextIOWrapper, chunk_size : int = 4) -> str:
     while True:
-        data = file_object.read(chunk_size)
+        data = file_object.readline(chunk_size)
+        #data = file_object.read(chunk_size)
         if not data:
             break
         else:
@@ -24,8 +25,25 @@ def get_binary_file():
 
 def symbols_statistic(byte_string_generator) -> Counter:
     meta_inf = Counter()
+    buf = b""
+    read_error = False
+    
     for chunk in byte_string_generator:
-        meta_inf.update(chunk.decode('utf-8', errors='ignore'))
+        try:
+            if read_error: 
+                decoded_text = (buf + chunk).decode("utf-8")
+            else:
+                decoded_text  = chunk.decode("utf-8")
+        except UnicodeDecodeError as E:
+            print(E)
+            buf += chunk
+            read_error = True
+            continue
+        
+        if read_error:
+            buf = b""
+            read_error = False
+        meta_inf.update(decoded_text)
     return meta_inf
 
 
