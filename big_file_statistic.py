2e8a14e3b153cc40abb801325a10675a68ca3c22
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2023-02-01 16:30:13 +0300

    Resolution

--- big_file_statistic.py
+++ big_file_statistic.py
@@ -3,10 +3,9 @@ from io import TextIOWrapper
 import sys
 
 
-def read_data_chunk(file_object: TextIOWrapper, chunk_size : int = 4) -> str:
+def read_data_chunk(file_object: TextIOWrapper, chunk_size: int = 33) -> str:
     while True:
-        data = file_object.readline(chunk_size)
-        #data = file_object.read(chunk_size)
+        data = file_object.read(chunk_size)
         if not data:
             break
         else:
@@ -20,26 +19,25 @@ def get_binary_file():
         raise RuntimeError("Path argument are not specified")
     path = sys.argv[1]
     file = open(path, 'rb')
-    return read_data_chunk(file) 
+    return read_data_chunk(file)
 
 
 def symbols_statistic(byte_string_generator) -> Counter:
     meta_inf = Counter()
     buf = b""
     read_error = False
-    
+
     for chunk in byte_string_generator:
         try:
-            if read_error: 
-                decoded_text = (buf + chunk).decode("utf-8")
+            if read_error:
+                decoded_text = (buf + chunk).decode("utf-8", errors="ignore")
             else:
-                decoded_text  = chunk.decode("utf-8")
+                decoded_text = chunk.decode("utf-8")
         except UnicodeDecodeError as E:
-            print(E)
             buf += chunk
             read_error = True
             continue
-        
+
         if read_error:
             buf = b""
             read_error = False
@@ -47,12 +45,13 @@ def symbols_statistic(byte_string_generator) -> Counter:
     return meta_inf
 
 
-def print_tables(table:Counter, top_count: int = 10):
+def print_tables(table: Counter, top_count: int = 10):
     print("Most common symbols are:")
     for smb, count in table.most_common(top_count):
         print(f"{repr(smb)} => {count} times")
 
+
 if __name__ == '__main__':
-    byte_string_gen  = get_binary_file()
+    byte_string_gen = get_binary_file()
     result = symbols_statistic(byte_string_gen)
     print_tables(result)
