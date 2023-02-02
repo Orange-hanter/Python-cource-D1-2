80920c39b4df388459081d67744a3b14dbec6a49
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2023-02-02 13:08:45 +0300

    New way to solve task

--- big_file_statistic.py
+++ big_file_statistic.py
@@ -3,55 +3,25 @@ from io import TextIOWrapper
 import sys
 
 
-def read_data_chunk(file_object: TextIOWrapper) -> str:
-    while True:
-        data = file_object.readline()
-        if not data:
-            break
-        else:
-            yield data
-    file_object.close()
-
-
-def get_binary_file():
-    # todo implement configuration of the number of displayed elements through arguments
-    if not len(sys.argv) == 2:
-        raise RuntimeError("Path argument are not specified")
-    path = sys.argv[1]
-    file = open(path, 'rb')
-    return read_data_chunk(file)
-
-
-def symbols_statistic(byte_string_generator) -> Counter:
-    meta_inf = Counter()
-    buf = b""
-    read_error = False
-
-    for chunk in byte_string_generator:
-        try:
-            if read_error:
-                decoded_text = (buf + chunk).decode("utf-8", errors="ignore")
-            else:
-                decoded_text = chunk.decode("utf-8")
-        except UnicodeDecodeError as E:
-            buf += chunk
-            read_error = True
-            continue
-
-        if read_error:
-            buf = b""
-            read_error = False
-        meta_inf.update(decoded_text)
+def symbols_statistic(byte_string: str, meta_inf: Counter) -> Counter:
+    decoded_text = (byte_string).decode("utf-8", errors="ignore")
+    meta_inf.update(decoded_text)
     return meta_inf
 
 
-def print_tables(table: Counter, top_count: int = 10):
+def print_tables(table: Counter, top_count: int = 10) -> None:
     print("Most common symbols are:")
     for smb, count in table.most_common(top_count):
         print(f"{repr(smb)} => {count} times")
 
 
 if __name__ == '__main__':
-    byte_string_gen = get_binary_file()
-    result = symbols_statistic(byte_string_gen)
-    print_tables(result)
+    if not len(sys.argv) == 2:
+        raise RuntimeError("Path argument are not specified")
+    meta_inf = Counter()
+    path = sys.argv[1]
+    with open(path, 'rb') as file:
+        for chunk in file.readlines():
+            symbols_statistic(chunk, meta_inf=meta_inf)
+
+    print_tables(meta_inf)
