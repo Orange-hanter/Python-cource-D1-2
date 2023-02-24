c0286040a1a5046969e3b2a2cdd360f16d17d2f7
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2023-02-24 14:57:55 +0300

    Wrong test conditions

--- CachMissBench.py
+++ CachMissBench.py
@@ -7,11 +7,13 @@ def miss_check(a, b):
     else:
         return 1
 
+print("Check 1:", miss_check(4, 5) == 2)
+print("Check 2:", miss_check(1, 5) == 1)
 
 for i in range(0, 10):
     repetitions = 10000000
     time1 = timeit.timeit(lambda: miss_check(4, 5), number=repetitions), "without"
-    time2 = timeit.timeit(lambda: miss_check(5, 4), number=repetitions), "with"
+    time2 = timeit.timeit(lambda: miss_check(1, 5), number=repetitions), "with"
 
     best, worst = (time1, time2) if time1 < time2 else (time2, time1)
 
