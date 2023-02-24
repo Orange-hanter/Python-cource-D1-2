0bf9fe2ab1ae62d14f99c2607f7f1ededa6691af
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2023-02-24 12:04:48 +0300

    fix

--- CachMissBench.py
+++ CachMissBench.py
@@ -1,24 +1,17 @@
 import timeit
 
 
-def no_miss(a, b):
+def miss_check(a, b):
     if (a/b) > 0.5:
         return 2
     else:
         return 1
 
 
-def do_miss(a, b):
-    if (a/b) < 0.5:
-        return 1
-    else:
-        return 2
-
-
 for i in range(0, 10):
     repetitions = 10000000
-    time1 = timeit.timeit(lambda: no_miss(4, 5), number=repetitions), "without"
-    time2 = timeit.timeit(lambda: do_miss(4, 5), number=repetitions), "with"
+    time1 = timeit.timeit(lambda: miss_check(4, 5), number=repetitions), "without"
+    time2 = timeit.timeit(lambda: miss_check(5, 4), number=repetitions), "with"
 
     best, worst = (time1, time2) if time1 < time2 else (time2, time1)
 
