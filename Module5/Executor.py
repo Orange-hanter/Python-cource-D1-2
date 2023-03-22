160163acba1b3377c90e02ed6759ee5cf782e3c8
Daniil Akhramiuk <Daniil_Akhramiuk@epam.com>
2023-03-22 19:13:51 +0300

    Portion of fixes * muted child side stdout * remove unused with on child side * proper way to print data on parent side

--- Module5/Executor.py
+++ Module5/Executor.py
@@ -2,28 +2,28 @@ import os
 import sys
 
 
-def print_stat():
-    pid = os.getpid()
-    print(f"pid:{pid}")
-    os.system("/bin/ls -l /proc/%s/fd" %pid)
-
-
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         raise RuntimeError("Path argument are not specified")
     cmd, args = sys.argv[1], sys.argv[2:]
-    
+
     cp_env = os.environ.copy()
     parent_conn, child_conn = os.pipe()
-    
+
     child_proc_id = os.fork()
-    if child_proc_id:    #Parent process
+    if child_proc_id:  # Parent process
         os.close(child_conn)
-        with os.fdopen(parent_conn, 'r') as inp:
-            print(inp.read())
+        parent_conn = os.fdopen(parent_conn, 'r')
+        while True:
+            data = parent_conn.readline()
+            if not data:
+                break
+            print("P:", data, end="")
+        parent_conn.close()
+
     else:
         os.close(parent_conn)
-        os.dup2(child_conn, 0)
         os.dup2(child_conn, 1)
-        with os.fdopen(child_conn, 'w') as out:
-            os.execve(cmd, args, cp_env)
+        os.dup2(child_conn, 2)
+        sys.stdout = open(os.devnull, 'w')
+        os.execve(cmd, args, cp_env)
