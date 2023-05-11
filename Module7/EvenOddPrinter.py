300231ec1539232b4d63781dfdc91a38414eace7
daniil <danil.bel53@gmail.com>
2023-05-11 21:17:04 +0300

    Multithreading task

---
+++ Module7/EvenOddPrinter.py
@@ -0,0 +1,47 @@
+import threading
+
+
+class NumberPrinter:
+    def __init__(self, max_num, /, start_with=0):
+        assert start_with < max_num
+
+        self._limit = max_num
+        self._start_num = start_with
+
+        self._lock = threading.Lock()
+        self._event_even = threading.Event()
+        self._event_odd = threading.Event()
+
+        self._isEven = int(self._start_num % 2)
+        if self._isEven:
+            self._event_even.set()
+            self._event_odd.clear()
+        else:
+            self._event_even.clear()
+            self._event_odd.set()
+
+    def print_even(self):
+        modifier = 1 if not self._isEven else 0
+        for i in range(self._start_num + modifier, self._limit, 2):
+            self._event_even.wait()
+            with self._lock:
+                print(i)
+                self._event_odd.set()
+            self._event_even.clear()
+
+    def print_odd(self):
+        modifier = 1 if self._isEven else 0
+        for i in range(self._start_num + modifier, self._limit, 2):
+            self._event_odd.wait()
+            with self._lock:
+                print(i)
+                self._event_even.set()
+            self._event_odd.clear()
+
+
+if __name__ == '__main__':
+    sync_printer = NumberPrinter(100, start_with=42)
+    t1 = threading.Thread(target=sync_printer.print_even)
+    t2 = threading.Thread(target=sync_printer.print_odd)
+    [threading.Thread.start(obj) for obj in [t1, t2]]
+    [threading.Thread.join(obj) for obj in [t1, t2]]
