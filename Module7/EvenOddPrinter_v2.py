4ce62e18baa0add70aacdde958cb5904e37fdca4
daniil <danil.bel53@gmail.com>
2023-05-14 20:40:49 +0300

    Clean code. Removed all unnecessary methods and some cleanup

--- Module7/EvenOddPrinter_v2.py
+++ Module7/EvenOddPrinter_v2.py
@@ -8,28 +8,30 @@ class NumberPrinter:
         self._limit = max_num
         self.index = start_with
 
-        self._lock_even = threading.Lock()
-        self._lock_odd = threading.Lock()
-        self._lock_odd.acquire()
+        self._lock_1 = threading.Lock()
+        self._lock_2 = threading.Lock()
 
     def print_num(self):
-        l2, l1 = (self._lock_even, self._lock_odd) if self._lock_even.locked() else (self._lock_odd, self._lock_even)
+        self._lock_2.acquire()
+        l1, l2 = (self._lock_1, self._lock_2) if not self._lock_1.locked() else (self._lock_2, self._lock_1)
+        if self._lock_1.locked():
+            self._lock_2.release()
         while True:
             l1.acquire()
-            print(self.index, threading.get_ident())
+            print(self.index)
             self.index += 1
             l2.release()
 
+            # Do not change. Need for small ranges like 0..1
             if self.index >= self._limit:
                 break
 
-    def _is_index_even(self):
-        return self.index % 2 == 0
-
 
 if __name__ == '__main__':
-    sync_printer = NumberPrinter(100, start_with=41)
+    sync_printer = NumberPrinter(100, start_with=1)
     t1 = threading.Thread(target=sync_printer.print_num)
     t2 = threading.Thread(target=sync_printer.print_num)
-    [threading.Thread.start(obj) for obj in [t1, t2]]
-    [threading.Thread.join(obj) for obj in [t1, t2]]
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
