637984872acba5ae49190ba42b8779d3fb9a38db
daniil <danil.bel53@gmail.com>
2023-05-15 15:36:54 +0300

    9

--- Module7/EvenOddPrinter.py
+++ Module7/EvenOddPrinter.py
@@ -6,39 +6,36 @@ class NumberPrinter:
         assert start_with < max_num
 
         self._limit = max_num
-        self._start_num = start_with
+        self._index = start_with
 
-        self._event_even = threading.Event()
-        self._event_odd = threading.Event()
-
-        self._isEven = int(self._start_num % 2)
-        if self._isEven:
-            self._event_even.set()
-            self._event_odd.clear()
-        else:
-            self._event_even.clear()
-            self._event_odd.set()
+        self._event_1 = threading.Event()
+        self._event_2 = threading.Event()
+        self._event_2.set()
 
     def print_even(self):
-        modifier = 1 if not self._isEven else 0
-        for i in range(self._start_num + modifier, self._limit, 2):
-            self._event_even.wait()
-            print(i, "even")
-            self._event_even.clear()
-            self._event_odd.set()
+        while True:
+            self._event_1.wait()
+            print(self._index)
+            self._index += 1
+            self._event_1.clear()
+            self._event_2.set()
+            if self._index >= self._limit: break
 
     def print_odd(self):
-        modifier = 1 if self._isEven else 0
-        for i in range(self._start_num + modifier, self._limit, 2):
-            self._event_odd.wait()
-            print(i, "odd")
-            self._event_odd.clear()
-            self._event_even.set()
+        while True:
+            self._event_2.wait()
+            print(self._index)
+            self._index += 1
+            self._event_2.clear()
+            self._event_1.set()
+            if self._index >= self._limit: break
 
 
 if __name__ == '__main__':
-    sync_printer = NumberPrinter(100, start_with=2)
+    sync_printer = NumberPrinter(1_000_000, start_with=0)
     t1 = threading.Thread(target=sync_printer.print_even)
     t2 = threading.Thread(target=sync_printer.print_odd)
-    [threading.Thread.start(obj) for obj in [t1, t2]]
-    [threading.Thread.join(obj) for obj in [t1, t2]]
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
