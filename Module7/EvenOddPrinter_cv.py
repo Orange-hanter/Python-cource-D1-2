014c2b30e8420f4d46fcc525a2214c3746b09408
daniil <danil.bel53@gmail.com>
2023-05-16 09:37:59 +0300

    Multiprocess realization

--- Module7/EvenOddPrinter_cv.py
+++ Module7/EvenOddPrinter_cv.py
@@ -12,9 +12,10 @@ class NumberPrinter:
         self.__cv_1 = threading.Condition()
 
     def print_num(self, predicate: Callable[[int], bool]):
+        _predicate = lambda: predicate(self.__index)
         while True:
             with self.__cv_1:
-                self.__cv_1.wait_for(lambda: predicate(self.__index))
+                self.__cv_1.wait_for(_predicate)
                 self.__business_logic()
                 self.__cv_1.notify()
 
@@ -29,8 +30,8 @@ class NumberPrinter:
 
 if __name__ == '__main__':
     sync_printer = NumberPrinter(1_000_000, start_with=0)
-    first_thread = threading.Thread(target=sync_printer.print_num, args=(lambda x: x % 2 == 0,))
-    second_thread = threading.Thread(target=sync_printer.print_num, args=(lambda x: not x % 2 == 0,))
+    first_thread = threading.Thread(target=sync_printer.print_num, args=(lambda x: not x % 2 == 0,))
+    second_thread = threading.Thread(target=sync_printer.print_num, args=(lambda x: x % 2 == 0,))
     first_thread.start()
     second_thread.start()
 
---
+++ Module7/EvenOddPrinter_mp.py
@@ -0,0 +1,22 @@
+from multiprocessing import Process, Lock, Value, Pool
+
+
+def print_num(index: Value, l1: Lock, l2: Lock):
+    while index.value < 1_000_000:
+        l1.acquire()
+        print(index.value)
+        index.value += 1
+        l2.release()
+
+
+if __name__ == '__main__':
+    local_index = Value('i', 0)
+    lock_1 = Lock()
+    lock_2 = Lock()
+    lock_2.acquire()
+    process1 = Process(target=print_num, args=(local_index, lock_1, lock_2))
+    process2 = Process(target=print_num, args=(local_index, lock_2, lock_1))
+    process1.start()
+    process2.start()
+    process1.join()
+    process2.join()
