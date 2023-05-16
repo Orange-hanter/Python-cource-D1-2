7092a12abdb9b1f4c75eec4e8d98d488fbf28fa7
daniil <danil.bel53@gmail.com>
2023-05-16 10:02:47 +0300

    Multiprocess realization without shared value

--- Module7/EvenOddPrinter_mp.py
+++ Module7/EvenOddPrinter_mp.py
@@ -1,21 +1,19 @@
-from multiprocessing import Process, Lock, Value
+from multiprocessing import Process, Lock
 
 
-def print_num(index: Value, l1: Lock, l2: Lock):
-    while index.value < 1_000_000:
+def print_num(start: int, l1: Lock, l2: Lock):
+    for i in range(start, 1_000_001, 2):
         l1.acquire()
-        print(index.value)
-        index.value += 1
+        print(i)
         l2.release()
 
 
 if __name__ == '__main__':
-    local_index = Value('i', 0)
     lock_1 = Lock()
     lock_2 = Lock()
     lock_2.acquire()
-    process1 = Process(target=print_num, args=(local_index, lock_1, lock_2))
-    process2 = Process(target=print_num, args=(local_index, lock_2, lock_1))
+    process1 = Process(target=print_num, args=(0, lock_1, lock_2))
+    process2 = Process(target=print_num, args=(1, lock_2, lock_1))
     process1.start()
     process2.start()
     process1.join()
